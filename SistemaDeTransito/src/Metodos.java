fac6cb72fbc1d2b4abf4e3c652725e1b34c86093
Lucas <lucaswar05@gmail.com>
2024-05-13 08:44:58 -0300

    RegistroEmArquivoTXT

--- SistemaDeTransito/src/Metodos.java
+++ SistemaDeTransito/src/Metodos.java
@@ -1,5 +1,7 @@
 import javax.swing.JOptionPane;
+import java.io.*;
 public class Metodos {
+    /* Criar um registro (sem salvar):
     public Estatistica[] adicionarRegistro(Estatistica[] cidade){
         // criar um registro baseado na classe Estatisticas.
         for(int i = 0; i < cidade.length;i++){
@@ -8,8 +10,46 @@ public class Metodos {
             cidade[i].quantidadeAcidentes = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de acidentes da " + (i+1) + "º cidade: "));
         }
         return cidade;
+    }*/
+    // criar registro (salvando em txt):
+    public Estatistica[] adicionarRegistro(Estatistica[] cidade) throws IOException{
+        String file = "REGISTROTRANSITO.txt"; // Escolhe o arquivo txt para gravação
+        BufferedWriter escrever = new BufferedWriter(new FileWriter(file)); // cria o "escrevedor"
+        for(int i = 0; i < cidade.length;i++){ // Loop de entrada
+            // código da cidade:
+            cidade[i].codigoCidade = Integer.parseInt(JOptionPane.showInputDialog("Digite o código da " + (i+1) + "º cidade: "));
+            escrever.write(Integer.toString(cidade[i].codigoCidade)); // registra o valor (int) como str em uma linha do txt
+            escrever.newLine(); // próxima linha
+            // Nome da cidade: 
+            cidade[i].nomeCidade = JOptionPane.showInputDialog("Digite o nome da " + (i+1) + "º cidade: ");
+            escrever.write(cidade[i].nomeCidade); // registra o valor (sem conversão)
+            escrever.newLine();
+            // Quantidade de Acidentes;
+            cidade[i].quantidadeAcidentes = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de acidentes da " + (i+1) + "º cidade: "));
+            escrever.write(Integer.toString(cidade[i].quantidadeAcidentes));
+            escrever.newLine();
+        }
+        escrever.close(); // termina a escrita, ----[NÃO ESQUECER]----
+        JOptionPane.showMessageDialog(null, "Gravação concluída.");
+        return cidade;
     }
-
+    // ler registro (Importante!)
+    public void lerRegistro(Estatistica[] cidade) throws IOException{
+        String file = "REGISTROTRANSITO.txt"; // Escolhe o arquivo para leitura
+        BufferedReader ler = new BufferedReader(new FileReader(file)); // cria  "leitor"
+        for(int i = 0; i < cidade.length;i++){
+            cidade[i] = new Estatistica();  // cria o construtor para leitura;
+        }
+        for(int i = 0; i < cidade.length;i++){
+            cidade[i].codigoCidade = Integer.parseInt(ler.readLine()); // ler str e converter (int)
+            cidade[i].nomeCidade = ler.readLine();
+            cidade[i].quantidadeAcidentes = Integer.parseInt(ler.readLine());
+        }
+        ler.close();
+        JOptionPane.showMessageDialog(null, "Leitura concluída.");
+        return;
+    }
+    // verificar cidades entrew 100 e 500 acidentes:
     public void verificarIntervalo(Estatistica[] cidade){
         int cidcont = 0; // contador de cidades que aparecem no intervalo.
         System.out.println("Cidades entre 100 e 500 acidentes: ");
@@ -24,8 +64,10 @@ public class Metodos {
             System.out.println("Nenhuma cidade está entre 100 e 500 acidentes.");
         }
         System.out.println("...");
+        // Mensagem de saída
+        JOptionPane.showMessageDialog(null, "Verificação concluída.");
     }
-
+    // verific
     public void maiorEMenor(Estatistica[] cidade){
         // definir variáveis:
         int maior = -1, menor = -1;
@@ -51,6 +93,8 @@ public class Metodos {
         System.out.println("Quantidade de acidentes: " + cidade[maiorci].quantidadeAcidentes);
         System.out.println("\nCidade com a menor quantidade de acidentes: " + cidade[menorci].nomeCidade);
         System.out.println("Quantidade de acidentes: " + cidade[menorci].quantidadeAcidentes);
+        // Mensagem de saída
+        JOptionPane.showMessageDialog(null, "Verificação concluída.");
     }
 
     public void maiorQueMedia(Estatistica[] cidade){
@@ -63,12 +107,13 @@ public class Metodos {
         }
         mediaTotal = mediaTotal/mediaQuant;
         // saída, cidades com maiores quant. de acidentes que a média
-        System.out.println("Nome e quantidade de acidente das cidade acima da média: \n");
+        System.out.println("\nNome e quantidade de acidente das cidade acima da média: \n");
         for(int ct = 0; ct < cidade.length; ct++){
             if(cidade[ct].quantidadeAcidentes > mediaTotal){
                 System.out.println(cidade[ct].nomeCidade + "\n" + cidade[ct].quantidadeAcidentes);
             }
         }
-
+        // Mensagem de saída
+        JOptionPane.showMessageDialog(null, "Verificação concluída.");
     }
 }
--- SistemaDeTransito/src/Principal.java
+++ SistemaDeTransito/src/Principal.java
@@ -1,39 +1,42 @@
+import java.io.IOException;
 import javax.swing.JOptionPane;
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException { // throws *** é por conta do registro em txt, ----[NÃO ESQUECER]----
         // Estancia de classe em # registros de vetor:
-        Estatistica[] cid = new Estatistica[10];
+        Estatistica[] cid = new Estatistica[2];
         // Estanciar os métodos (função/procedimentos):
         Metodos metodos = new Metodos();
         // Executar o construtor:
         for(int i = 0; i < cid.length; i++){
             cid[i] = new Estatistica();
         }
-        // Definir variáveis:
         int loopEntrada = 0;
-        // Loop-Início:
         while (loopEntrada != 9) {
             loopEntrada = Integer.parseInt(JOptionPane.showInputDialog(
-                "MENU ESTATÍSTICA\n[1] - Cadastro de estatística.\n[2] - Consulta por quantidade de acidente.\n[3] - Consulta por estatística de acidente.\n[4] - Acidentes acima da média.\n[9] - Finalizar programa."));
+                "MENU ESTATÍSTICA\n[1] - Cadastro de estatística no registro.\n[2] - Ler estatísticas do registro.\n[3] - Consulta por quantidade de acidente.\n[4] - Consulta por estatística de acidente.\n[5] - Acidentes acima da média.\n[9] - Finalizar programa."));
                 switch (loopEntrada) {
                     case 1:
                         // Executa a função de registro 
                         metodos.adicionarRegistro(cid);
                         break;
                     case 2:
-                        // Executa o procedimento para verificar a quantidade de acidentes entre 100 e 500
-                        metodos.verificarIntervalo(cid);
+                        // Executar função leitura
+                        metodos.lerRegistro(cid);
                         break;
                     case 3:
                         // Executa o procedimento para verificar a quantidade de acidentes entre 100 e 500
-                        metodos.maiorEMenor(cid);
+                        metodos.verificarIntervalo(cid);
                         break;
                     case 4:
+                        // Executa o procedimento para verificar a quantidade de acidentes entre 100 e 500
+                        metodos.maiorEMenor(cid);
+                        break;
+                    case 5:
                         // Executa o procedimento para mostrar quais cidades estão acicma da média de acidentes
                         metodos.maiorQueMedia(cid);
                         break;
                     case 9:
-                    // saída
+                        // saída
                         break;
                     default:
                         JOptionPane.showMessageDialog(null,"Opção inválida");
