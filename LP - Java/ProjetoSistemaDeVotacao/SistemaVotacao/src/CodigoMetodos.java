1ef4c743dc23844e5b362ad357e80d28b8bc680c
Lucas <lucaswar05@gmail.com>
2024-05-16 13:25:51 -0300

    Atualização do projeto 16/05

--- LP - Java/ProjetoSistemaDeVotacao/SistemaVotacao/src/CodigoMetodos.java
+++ LP - Java/ProjetoSistemaDeVotacao/SistemaVotacao/src/CodigoMetodos.java
@@ -14,14 +14,66 @@ public class CodigoMetodos {
         return voto;
     }
     // Guardar os registro em uma matrix 2x200:
-    public int[][] classificaregistro(Votacao[] voto, int[][] registro){
+    public int[][] classificaregistro(Votacao[] votos, int[][] registro){
+        for(int c = 0 ; c < votos.length ; c ++){
+            registro[c][0] = votos[c].numeroSecao;
+            registro[c][1] = votos[c].numeroCandidato;   
+        }
+        JOptionPane.showMessageDialog(null,
+        "Classificação concluída.");
         return registro;
     }
     // Gravar/Ler registro:
-    public int[][] consultarRegistro(int[][] registro) throws IOException{
-        //String registroFisico = "Registro_Votacao.txt";
-        //BufferedWriter ler = new BufferedWriter(new FileWriter(registroFisico));
-        
-        return registro;
+    public void registro(Votacao[] voto, int[][] registro) throws IOException{
+        // Variável de controle:
+        int opc=0;
+        // Entrada de valor:
+        while(opc != 1 && opc != 2){
+            opc = Integer.parseInt(JOptionPane.showInputDialog(
+                "ESCOLHA " +
+                "\n[1] - Gravar. " +
+                "\n[2] - Ler. " ));
+            switch (opc) {
+                case 1:
+                    registroGravar(registro);
+                    break;
+                case 2:
+                    registroLer(voto, registro);
+                    break;
+                default:
+                JOptionPane.showMessageDialog(null,
+                    "opção inválida, tente novamente.");
+                    break;
+            }
+        }
+    }
+    // Gravar o registro em .txt:
+    static void registroGravar(int[][] reg) throws IOException{
+        String registroFisico = "Registro_Votacao.txt";
+        BufferedWriter escrever = new BufferedWriter(new FileWriter(registroFisico));
+        for(int i = 0 ; i < reg.length ; i++){ // linha
+            for(int j = 0 ; j < reg[0].length ; j++){ // coluna
+                escrever.write(Integer.toString(reg[i][j])); // registra o valor (int) como str em uma linha do txt
+                escrever.newLine(); // próxima linha
+            }
+        }
+        escrever.close();
+        JOptionPane.showMessageDialog(null, "Gravação concluída.");
+    }
+    // Ler o registro .txt:
+    static void registroLer(Votacao[] voto, int[][] reg) throws IOException{
+        String registroFisico = "Registro_Votacao.txt";
+        BufferedReader ler = new BufferedReader(new FileReader(registroFisico));
+        for(int i = 0 ; i < reg.length ; i++){ // linha
+            for(int j = 0 ; j < reg[0].length ; j++){ // coluna
+                if (i == 0 || j == 0){ // condicional pra gravar a 1º linha (não sei pq precisa mas só funcionou desse jeito)
+                    voto[0].numeroSecao = reg[0][0];
+                }
+                reg[i][j] = (Integer.parseInt(ler.readLine()));
+            }
+            voto[i].numeroSecao = reg[i][0];
+            voto[i].numeroCandidato = reg[i][1];
+        }
+        ler.close(); 
     }
 }
--- LP - Java/ProjetoSistemaDeVotacao/SistemaVotacao/src/CodigoPrincipal.java
+++ LP - Java/ProjetoSistemaDeVotacao/SistemaVotacao/src/CodigoPrincipal.java
@@ -3,9 +3,9 @@ import javax.swing.JOptionPane;
 public class CodigoPrincipal {
     public static void main(String[] args) throws IOException{
         // variável global:
-        int[][] registroVotacao = new int[199][1];
+        int[][] registroVotacao = new int[200][2];
         // Instancia de Classe:
-        Votacao[] voto = new Votacao[199]; // Classe Votação
+        Votacao[] voto = new Votacao[200]; // Classe Votação
         CodigoMetodos metodo = new CodigoMetodos(); // Metodos
         // Executar o constutor:
         for (int c = 0; c < voto.length; c++ ) {
@@ -23,14 +23,14 @@ public class CodigoPrincipal {
                 "\n[4] - Mostrar Indicadores." +
                 "\n[9] - Finalizar Programa."));
             switch (loopMenuVotacao) {
-                case 1: // 
+                case 1: // Carregar votos e número da seção
                     metodo.carregaSecao(voto);
                     break;
                 case 2: // Guardar os registros em 1 matriz:
                     metodo.classificaregistro(voto, registroVotacao);
                     break;
                 case 3: // Gravar/Ler dados
-                    metodo.consultarRegistro(registroVotacao);
+                    metodo.registro(voto, registroVotacao);
                     break;
                 case 4:
                     // Mostrar menu estatísticas:
@@ -58,21 +58,22 @@ public class CodigoPrincipal {
                 "\n[3] - Quantidade de votos por candidato." +
                 "\n[4] - 10 primeiros colocados (nº do candidato e sua quantidade de votos)." +
                 "\n[9] - Voltar ao menu principal."));
-                switch (loopMenuIndicadores) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 9:
-                        break;
-                    default:
-                        JOptionPane.showMessageDialog(null,"Opção Inválida.");
-                        break;
-                }
+            switch (loopMenuIndicadores) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 9:
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(
+                        null,"Opção Inválida.");
+                    break;
+            }
         }
     }
 }
