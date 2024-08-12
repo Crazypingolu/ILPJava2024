f0d666c86ca1126b8669fd2ee0bd5ae2b22cf6fa
Lucas <lucaswar05@gmail.com>
2024-08-12 14:42:05 -0300

    Exercício 08

--- LP - Java/ExerciciosDaLista/lote1p1/Ex08L1p1.java
+++ LP - Java/ExerciciosDaLista/lote1p1/Ex08L1p1.java
@@ -1,3 +1,20 @@
+/* 
+ * Programa: Receba o valor de um depósito em poupança.
+ * Calcule e mostre o valor após 1 mês de aplicação sabendo que rende 1,3% a. m.
+ * Programador: LucasP, Crazypingolu
+ * versão: 2.0
+*/
+// Trazer bibliotecas:
+import javax.swing.JOptionPane;
 public class Ex08L1p1 {
-    
+    public static void main(String[] args) {
+        // Declarar variáveis:
+        double val0, valT;
+        // Entrada de dados:
+        val0 = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor depositado:"));
+        // Processamento de dados:
+        valT = val0 * 1.013;
+        // Saída de dados:
+        System.out.println("Valor (aproximado) após 1 mês: \n" + valT);
+    }
 }
